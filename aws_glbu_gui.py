a1c2bab34e1394b385009fa927261f9b7d9d0125
paradoxmiller <33523467+paradoxmiller@users.noreply.github.com>
2018-05-12 08:35:14 -0700

    Adding colors to text
    Adding colors to the text and cleaning the output a bit

--- aws_glbu_gui.py
+++ aws_glbu_gui.py
@@ -1,22 +1,39 @@
-#!/usr/bin/env python3
-import sys
-import os
 from tkinter import *
+import os
 
-def launch_glbu():
-   print("Sending archive %s to AWS Glacier..." % (e1.get()))
-   os.system('aws glacier upload-archive --account-id - --vault-name examplevault --body '+(e1.get()))
+class bcolors:
+    fail = '\033[91m'
+    warn = '\033[93m'
+
+def send_aws_bu():
+  os.system('clear')
+  if e1.get()=="":
+    print (bcolors.fail +"\nNo input detected.  Please input a valid path and file name.")
+     
+  else:
+    os.system('printf "\033[0m"')
+    os.system('clear')
+    print("\nUploading archive %s to AWS Glacier..." % (e1.get()))
+    #os.system('aws glacier upload-archive --account-id - --vault-name my-vault --body ' +(e1.get)())
+    print(bcolors.warn +"\nFunction to upload is not enabled.  \nPlease enable the function in order to upload archive to Glacier.")
+	#print("Pinging... %s" % (e1.get()))
+	#os.system('ping '+(e1.get)()) #Could configure this to do other tasks
+
+def masterquit():
+  os.system('clear')
+  os.system('printf "\033[0m"')
+  quit()
 
 master = Tk()
-Label(master, font=("helvetica", "14", "bold"), text="Input Archive Name").grid(row=0)
+Label(master, text="Please input the archive name: ", bd="6", font=("helvetica", "12", "bold")).grid(row=0)
 
 e1 = Entry(master)
 
 e1.grid(row=0, column=1)
 
-Button(master, text='Quit', fg="red", bd="5", font=("helvetica", "14", "bold"), activebackground="orange", command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
-Button(master, text='Send Archive',fg="green", bd="5", font=("helvetica", "14", "bold"), activebackground="orange", command=launch_glbu).grid(row=3, column=1, sticky=W, pady=4)
+Button(master, text='QUIT', fg="red", bd="6", font=("helvetica", "14", "bold"),activebackground="orange", width="20", relief="raised", command=masterquit).grid(row=3, column=0, sticky=W, pady=4, padx=4)
+Button(master, text='Upload Archive', fg="green", bd="6", font=("helvetica", "14", "bold"),activebackground="orange", width="20", relief="raised", command=send_aws_bu).grid(row=3, column=1, sticky=W, pady=4, padx=4)
 
 master.title("AWS Glacier Backup GUI")
-master.geometry("400x150+250+225")
+master.geometry("530x150+250+225")
 mainloop( )
